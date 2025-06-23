90238380c2406a5f0f62508d70b2f944d6419af4
saket-shukla-0909 <shukla.kumar.saket@gmail.com>
2025-06-23 17:43:50 +0530

    daisy ui tailwind css correction

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
+  safelist: [
+    "chat",
+    "chat-start",
+    "chat-end",
+    "chat-bubble",
+    "chat-bubble-info",
+    "bg-primary",
+    "bg-base-200",
+    "text-white",
+    "text-base-content"
+  ],
   theme: {
     extend: {},
   },
