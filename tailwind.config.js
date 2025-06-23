5baf9113d0c1024226918223fb4fb45198b55b09
saket-shukla-0909 <shukla.kumar.saket@gmail.com>
2025-06-23 18:05:53 +0530

    chore: updated frontend with tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,7 @@
+// tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/daisyui/dist/**/*.js", // ✅ include DaisyUI components
-  ],
-  safelist: [
-    {
-      pattern: /chat(-start|-end|-bubble|-bubble-info)?/, // ✅ safer match
-    },
-    {
-      pattern: /(bg|text)-(primary|base-200|white|base-content)/,
-    },
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
