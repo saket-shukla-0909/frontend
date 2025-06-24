10ec281cdcace769be38a88ee2535cbbd15a8b2d
saket-shukla-0909 <shukla.kumar.saket@gmail.com>
2025-06-24 09:16:55 +0530

    tailwind commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
+  safelist: [
+  "chat", "chat-start", "chat-end", "chat-bubble", "chat-bubble-info",
+  "bg-white", "bg-gray-900", "text-white"
+  ],
+
   theme: {
     extend: {},
   },
