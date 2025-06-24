92f0d96a790d8ecb7fce4b86d624939b2aa1a018
saket-shukla-0909 <shukla.kumar.saket@gmail.com>
2025-06-24 20:15:05 +0530

    index html add in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // very important
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // ✅ All React component files
+    "./public/index.html",         // Optional but safe
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")], // use require()
+  plugins: [require("daisyui")],
 };
