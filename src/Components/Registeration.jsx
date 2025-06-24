ab62e88b56af0fa870e4272b8b4f7ba421b34c65
saket-shukla-0909 <shukla.kumar.saket@gmail.com>
2025-06-24 22:39:20 +0530

    useEffect

--- src/Components/Registeration.jsx
+++ src/Components/Registeration.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { loginUser, registerUser } from "../redux/authThunks";
