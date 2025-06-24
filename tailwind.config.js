87bbb6473a5b824c0ba8810b26ec12dd0040ab1c
saket-shukla-0909 <shukla.kumar.saket@gmail.com>
2025-06-24 20:10:46 +0530

    ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // ✅ VERY IMPORTANT
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // very important
   theme: {
     extend: {},
   },
-  plugins: [daisyui], // ✅ If you're using daisyUI, else remove
+  plugins: [require("daisyui")], // use require()
 };
