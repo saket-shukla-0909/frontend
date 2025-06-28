0fdf6a96172daa20a7482006ed3566e39cda868d
saket-shukla-0909 <shukla.kumar.saket@gmail.com>
2025-06-28 23:34:11 +0530

    correction authencticate

--- src/Home/Left/Left.jsx
+++ src/Home/Left/Left.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FiLogOut } from "react-icons/fi";
-import { FaUserCircle } from "react-icons/fa";
+import { FaUserCircle, FaEdit } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { logoutUser, uploadProfilePicture } from "../../redux/authThunks";
@@ -8,19 +8,14 @@ import { logoutUser, uploadProfilePicture } from "../../redux/authThunks";
 const Left = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
   const { user } = useSelector((state) => state.auth);
   const [showModal, setShowModal] = useState(false);
 
   const handleLogout = () => {
     dispatch(logoutUser())
       .unwrap()
-      .then(() => {
-        navigate("/Login");
-      })
-      .catch((err) => {
-        console.error("Logout failed:", err);
-      });
+      .then(() => navigate("/Login"))
+      .catch((err) => console.error("Logout failed:", err));
   };
 
   const handleFileUpload = (e) => {
@@ -43,26 +38,36 @@ const Left = () => {
         <div />
 
         <div className="flex flex-col items-center gap-6 mb-4">
+          {/* Logout Button */}
           <button title="Logout" onClick={handleLogout}>
             <FiLogOut className="text-3xl hover:text-red-300 transition-all" />
           </button>
 
-          {/* Show user profile or upload icon */}
-          {user?.profile_picture ? (
-            <img
-              src={`${process.env.REACT_APP_BASE_URL}/${user.profile_picture.replace(/\\/g, "/")}`}
-              alt="Profile"
-              className="w-10 h-10 rounded-full object-cover border border-gray-300"
-            />
-          ) : (
-            <button onClick={() => setShowModal(true)} title="Upload Photo">
-              <FaUserCircle className="text-3xl text-gray-600" />
-            </button>
-          )}
+          {/* Profile picture with edit functionality */}
+          <div className="relative group">
+            {user?.profile_picture ? (
+              <>
+                <img
+                  src={`${process.env.REACT_APP_BASE_URL}/${user.profile_picture.replace(/\\/g, "/")}`}
+                  alt="Profile"
+                  className="w-10 h-10 rounded-full object-cover border border-gray-300 cursor-pointer"
+                  onClick={() => setShowModal(true)}
+                />
+                <FaEdit
+                  className="absolute bottom-0 right-0 bg-white rounded-full text-gray-600 text-xs p-[2px] border border-gray-300 cursor-pointer group-hover:opacity-100 opacity-0 transition-opacity"
+                  onClick={() => setShowModal(true)}
+                />
+              </>
+            ) : (
+              <button onClick={() => setShowModal(true)} title="Upload Photo">
+                <FaUserCircle className="text-3xl text-gray-600" />
+              </button>
+            )}
+          </div>
         </div>
       </div>
 
-      {/* Upload Modal */}
+      {/* Modal for Upload */}
       {showModal && (
         <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50">
           <div className="bg-white w-[250px] h-[250px] rounded-xl shadow-lg flex flex-col items-center justify-center gap-4 p-4 relative">
